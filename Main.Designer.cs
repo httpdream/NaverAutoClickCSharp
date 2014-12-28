46f049fdfc026b483582de58aa48b841160a8c1f
hanc1208 <hanc1208@naver.com>
2014-12-28 15:26:00 +0900

    Main Design Changed

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -63,9 +63,9 @@
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonDeleteSelected = new System.Windows.Forms.Button();
+            this.buttonShowAll = new System.Windows.Forms.Button();
+            this.buttonShowProcessing = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -472,50 +472,50 @@
             // 
             // panel6
             // 
-            this.panel6.Controls.Add(this.button1);
-            this.panel6.Controls.Add(this.button2);
-            this.panel6.Controls.Add(this.button3);
+            this.panel6.Controls.Add(this.buttonDeleteSelected);
+            this.panel6.Controls.Add(this.buttonShowAll);
+            this.panel6.Controls.Add(this.buttonShowProcessing);
             this.panel6.Location = new System.Drawing.Point(12, 771);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(312, 33);
             this.panel6.TabIndex = 8;
             // 
-            // button1
+            // buttonDeleteSelected
             // 
-            this.button1.BackColor = System.Drawing.Color.SkyBlue;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 33);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "선택 삭제";
-            this.button1.UseVisualStyleBackColor = false;
+            this.buttonDeleteSelected.BackColor = System.Drawing.Color.SkyBlue;
+            this.buttonDeleteSelected.ForeColor = System.Drawing.Color.White;
+            this.buttonDeleteSelected.Location = new System.Drawing.Point(0, 0);
+            this.buttonDeleteSelected.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.buttonDeleteSelected.Name = "buttonDeleteSelected";
+            this.buttonDeleteSelected.Size = new System.Drawing.Size(100, 33);
+            this.buttonDeleteSelected.TabIndex = 2;
+            this.buttonDeleteSelected.Text = "선택 삭제";
+            this.buttonDeleteSelected.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // buttonShowAll
             // 
-            this.button2.BackColor = System.Drawing.Color.SkyBlue;
-            this.button2.Enabled = false;
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(106, 0);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 33);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "전체보기";
-            this.button2.UseVisualStyleBackColor = false;
+            this.buttonShowAll.BackColor = System.Drawing.Color.SkyBlue;
+            this.buttonShowAll.Enabled = false;
+            this.buttonShowAll.ForeColor = System.Drawing.Color.White;
+            this.buttonShowAll.Location = new System.Drawing.Point(106, 0);
+            this.buttonShowAll.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.buttonShowAll.Name = "buttonShowAll";
+            this.buttonShowAll.Size = new System.Drawing.Size(100, 33);
+            this.buttonShowAll.TabIndex = 3;
+            this.buttonShowAll.Text = "전체보기";
+            this.buttonShowAll.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // buttonShowProcessing
             // 
-            this.button3.BackColor = System.Drawing.Color.SkyBlue;
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(212, 0);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 33);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "진행만 보기";
-            this.button3.UseVisualStyleBackColor = false;
+            this.buttonShowProcessing.BackColor = System.Drawing.Color.SkyBlue;
+            this.buttonShowProcessing.ForeColor = System.Drawing.Color.White;
+            this.buttonShowProcessing.Location = new System.Drawing.Point(212, 0);
+            this.buttonShowProcessing.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.buttonShowProcessing.Name = "buttonShowProcessing";
+            this.buttonShowProcessing.Size = new System.Drawing.Size(100, 33);
+            this.buttonShowProcessing.TabIndex = 4;
+            this.buttonShowProcessing.Text = "진행만 보기";
+            this.buttonShowProcessing.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -530,6 +530,7 @@
             // 
             // Main
             // 
+            this.AcceptButton = this.buttonSearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
@@ -593,9 +594,9 @@
         private System.Windows.Forms.TextBox textBoxStart;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonDeleteSelected;
+        private System.Windows.Forms.Button buttonShowAll;
+        private System.Windows.Forms.Button buttonShowProcessing;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label5;
